4b3e0426c81400238331b6cca405522fd9ed4c10
Asif ps <asifdua7@gmail.com>
2026-07-24 16:41:25 +0530

    Create output.tf

---
+++ EC2-project- (VPC-subnet-sg)/output.tf
@@ -0,0 +1,21 @@
+#Expose important details like the EC2 public IP, instance ID, and S3 bucket attributes.
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web_server.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "bucket_id" {
+  description = "The name (ID) of the S3 bucket"
+  value       = aws_s3_bucket.my_bucket.id
+}
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.my_bucket.arn
+}
